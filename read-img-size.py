0e9b1fa8b694a0bdac3d4e7ef7b5ef3c92af4af4
ari moelhadi <fakhri.moelhadi@gmail.com>
2018-05-29 12:15:03 +0800

    Update print image
    Co-Authored-By: ari <fakhri.moelhadi@gmail.com>

--- read-img-size.py
+++ read-img-size.py
@@ -11,16 +11,16 @@ def main():
     dir_user = sorted(os.listdir(user_input))
 
     #print(dir_user)
-    
+
     for image_path in dir_user:
        if not image_path.startswith('.'):
-                
+
        # join folder and file path
         input_path = os.path.join(user_input, image_path)
-        
+
        # get the size and print filename and size
         image_size = Image.open(input_path).size
-        print('Image name: ', input_path, '\n', 'Size (Width, Height): ', image_size)
-       
+        print('Image name: ', input_path, image_size)
+
 if __name__ == '__main__':
     main()
